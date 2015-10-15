86557b38f0cac6ab662c437e3b389c0ff50ccc3a
Josewer <jose.luis.torregrosa1011@gmail.com>
2015-10-15 13:12:00 +0200

    Añado tabs y listview

--- AndroidChat/app/src/main/java/com/example/jose/androidchat/MainActivity.java
+++ AndroidChat/app/src/main/java/com/example/jose/androidchat/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.jose.androidchat;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private ListView listaChats;
+    private String[] chats = {"Juan", "Ana", "Pedro", "Silvana", "Miguel",
+            "Lucas"};
+
+    private ListView listaContactos;
+    private String[] contactos = {"Juan", "Ana", "Pedro", "Silvana", "Miguel",
+            "Lucas", "Kira", "Abel", "Jose", "Pablo"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,6 +35,71 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Resources res = getResources();
+
+        TabHost tabs=(TabHost)findViewById(android.R.id.tabhost);
+        tabs.setup();
+
+        TabHost.TabSpec spec=tabs.newTabSpec("mitab1");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("CHATS",
+                res.getDrawable(android.R.drawable.ic_btn_speak_now));
+        tabs.addTab(spec);
+
+        spec=tabs.newTabSpec("mitab2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("CONTACTOS",
+                res.getDrawable(android.R.drawable.ic_dialog_map));
+        tabs.addTab(spec);
+
+        tabs.setCurrentTab(0);
+
+        tabs.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                Log.i("AndroidTabsDemo", "Pulsada pestaña: " + tabId);
+            }
+        });
+
+
+        // Chats
+
+        listaChats = (ListView)findViewById(R.id.lvChats);
+
+        ArrayAdapter<String> adaptadorChats = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, chats);
+
+        listaChats.setAdapter(adaptadorChats);
+
+        listaChats.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(), "Ha pulsado el item " + position, Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+
+        // Contactos
+
+        listaContactos = (ListView)findViewById(R.id.lvContactos);
+
+        ArrayAdapter<String> adaptadorContactos = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contactos);
+
+        listaContactos.setAdapter(adaptadorContactos);
+
+        listaContactos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(), "Ha pulsado el item " + position, Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
     }
 
     @Override
